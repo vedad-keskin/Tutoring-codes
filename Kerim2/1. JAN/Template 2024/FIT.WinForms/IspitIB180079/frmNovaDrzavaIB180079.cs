69bb07f17ae346169532e6983a363e34a1bf3902
Vedad Keskin <vedad.keskin98@gmail.com>
2025-02-12 21:34:32 +0100

    Kodovi studenata sa K

---
+++ Kerim2/1. JAN/Template 2024/FIT.WinForms/IspitIB180079/frmNovaDrzavaIB180079.cs
@@ -0,0 +1,116 @@
+﻿using FIT.Data;
+using FIT.Data.IspitIB180079;
+using FIT.Infrastructure;
+using FIT.WinForms.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIT.WinForms.IspitIB180079
+{
+    public partial class frmNovaDrzavaIB180079 : Form
+    {
+        DLWMSDbContext db = new DLWMSDbContext();
+        private DrzaveIB180079 odabranaDrzava;
+
+        // za dodavanje nove drzave
+        public frmNovaDrzavaIB180079()
+        {
+            InitializeComponent();
+
+        }
+
+        // za editovanje postojece drzave
+        public frmNovaDrzavaIB180079(DrzaveIB180079 odabranaDrzava)
+        {
+            InitializeComponent();
+            this.odabranaDrzava = odabranaDrzava;
+        }
+
+        private void pbZastava_DoubleClick(object sender, EventArgs e)
+        {
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // C:\Users\ASUS\Desktop\C# Repos\Slike helpers\egy.jpg
+                pbZastava.Image = Image.FromFile(openFileDialog1.FileName);
+            }
+
+        }
+
+        private void btnSacuvaj_Click(object sender, EventArgs e)
+        {
+
+            if (Validiraj())
+            {
+                var zastava = Ekstenzije.ToByteArray(pbZastava.Image);
+                var naziv = txtNaziv.Text;
+                var status = chbStatus.Checked; // false true
+
+
+                if(odabranaDrzava == null)
+                {
+                    var novaDrzava = new DrzaveIB180079()
+                    {
+                        Naziv = naziv,
+                        Status = status,
+                        Zastava = zastava
+
+                    };
+
+                    db.DrzaveIB180079.Add(novaDrzava);
+
+                }
+                else
+                {
+
+                    odabranaDrzava.Status = status;
+                    odabranaDrzava.Naziv = naziv;
+                    odabranaDrzava.Zastava = zastava;
+
+                    db.DrzaveIB180079.Update(odabranaDrzava);
+
+                }
+
+                db.SaveChanges();
+
+                DialogResult = DialogResult.OK;
+            }
+
+        }
+
+        private bool Validiraj()
+        {
+            return
+                Helpers.Validator.ProvjeriUnos(pbZastava, err, Kljucevi.ReqiredValue) &&
+                Validator.ProvjeriUnos(txtNaziv, err, Kljucevi.ReqiredValue)
+                ;
+        }
+
+        private void frmNovaDrzavaIB180079_Load(object sender, EventArgs e)
+        {
+            UcitajInfo();
+        }
+
+        private void UcitajInfo()
+        {
+            
+            if(odabranaDrzava != null)
+            {
+                // BYTE[] -> IMAGE
+                pbZastava.Image = Ekstenzije.ToImage(odabranaDrzava.Zastava);
+
+                txtNaziv.Text = odabranaDrzava.Naziv;
+                chbStatus.Checked = odabranaDrzava.Status;
+
+            }
+
+        }
+    }
+}
---
+++ Kerim2/2. FEB/Template 2024/FIT.WinForms/IspitIB180079/frmNastavaIB180079.cs
@@ -0,0 +1,106 @@
+﻿using FIT.Data.IspitIB180079;
+using FIT.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIT.WinForms.IspitIB180079
+{
+    public partial class frmNastavaIB180079 : Form
+    {
+        private ProstorijeIB180079 odabranaProstorija; // AMF 6 -> 1
+        DLWMSDbContext db = new DLWMSDbContext();
+        List<NastavaIB180079> nastave;
+
+        public frmNastavaIB180079(ProstorijeIB180079 odabranaProstorija)
+        {
+            InitializeComponent();
+            this.odabranaProstorija = odabranaProstorija;
+        }
+
+        private void frmNastavaIB180079_Load(object sender, EventArgs e)
+        {
+            dgvNastave.AutoGenerateColumns = false;
+
+            lblNazivProstorije.Text = $"{odabranaProstorija.Naziv} - {odabranaProstorija.Oznaka}";
+
+
+            cbPredmet.DataSource = db.Predmeti.ToList();
+
+            cbDan.SelectedIndex = 0;
+            cbVrijeme.SelectedIndex = 0;
+
+            UcitajNastave();
+        }
+
+        private void UcitajNastave()
+        {
+            nastave = db.NastavaIB180079
+                //                        == 1
+                .Where(x => x.ProstorijaId == odabranaProstorija.Id)
+                .ToList();
+
+
+            if (nastave != null)
+            {
+
+                dgvNastave.DataSource = null;
+                dgvNastave.DataSource = nastave;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // kada uzimas iz baze
+            var predmet = cbPredmet.SelectedItem as PredmetiIB180079;
+
+            // kada uzimas rucno unesene
+            var dan = cbDan.SelectedItem.ToString(); // "Ponedeljak"
+            var vrijeme = cbVrijeme.SelectedItem.ToString(); // "08 - 10"
+
+            if (nastave.Exists(x=> x.Dan == dan && x.Vrijeme == vrijeme ))
+            {
+                MessageBox.Show("Nastava već postoji na tom terminu","Upozorenje",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+            else
+            {
+                var novaNastava = new NastavaIB180079()
+                {
+                    //Id = 1, // ovo puca na bazi
+                    //Predmet = predmet // ovo takodjer puca 
+
+                    Dan = dan,
+                    Vrijeme = vrijeme,
+                    PredmetId = predmet.Id,
+                    ProstorijaId = odabranaProstorija.Id,
+                    Oznaka = $"{predmet.Naziv} :: {dan} :: {vrijeme}"
+
+                };
+
+                db.NastavaIB180079.Add(novaNastava);
+                db.SaveChanges();
+
+            }
+
+
+
+            UcitajNastave();
+
+            
+
+
+
+        }
+
+        private void frmNastavaIB180079_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+    }
+}
