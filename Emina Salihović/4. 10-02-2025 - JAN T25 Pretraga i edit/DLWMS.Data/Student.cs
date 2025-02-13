bf9dd0e0118221a3ebc0b381e93ab6eac42965ab
Vedad Keskin <vedad.keskin98@gmail.com>
2025-02-13 08:32:58 +0100

    categorizing

---
+++ Emina Salihović/4. 10-02-2025 - JAN T25 Pretraga i edit/DLWMS.Data/Student.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DLWMS.Data
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+        public string Email { get; set; }
+        public string BrojIndeksa { get; set; }
+        public string Lozinka { get; set; }
+        public int GradId { get; set; }
+
+        public Grad Grad { get; set; } // .. override tost.. 
+        public int SpolId { get; set; }
+        public Spol Spol { get; set; }
+
+        public byte[] Slika { get; set; }
+        public bool Aktivan { get; set; }
+
+
+        // CUSTOM Paramteri
+        // ako su nam dostupni
+        // => .. i radimo ih odma 
+
+
+        public string StudentInfo => $"({BrojIndeksa}) {Ime} {Prezime}" ?? "N/A";
+
+        public string DrzavaInfo => Grad?.Drzava?.Naziv ?? "N/A";
+
+        //public string GradInfo => Grad?.Naziv ?? "N/A";
+        //public string SpolInfo => Spol?.Naziv ?? "N/A";
+
+
+        // ako nam nisu dostupni paramteri koji se traze 
+        // radimo NotMapped i popunjavamo ga preko for petlje u formi
+
+
+        public override string ToString()
+        {
+            return $"{Ime} {Prezime}";
+        }
+
+    }
+}
