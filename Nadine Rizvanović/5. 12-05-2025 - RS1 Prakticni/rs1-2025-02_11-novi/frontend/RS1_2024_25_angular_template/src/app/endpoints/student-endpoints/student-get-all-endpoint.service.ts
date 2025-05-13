031a78ea6f2f19dc11250cc2a8679b0999c1a7da
Vedad Keskin <vedad.keskin98@gmail.com>
2025-05-13 11:28:02 +0200

    1A - Nadine Rizvanovic

--- Nadine Rizvanović/5. 12-05-2025 - RS1 Prakticni/rs1-2025-02_11-novi/frontend/RS1_2024_25_angular_template/src/app/endpoints/student-endpoints/student-get-all-endpoint.service.ts
+++ Nadine Rizvanović/5. 12-05-2025 - RS1 Prakticni/rs1-2025-02_11-novi/frontend/RS1_2024_25_angular_template/src/app/endpoints/student-endpoints/student-get-all-endpoint.service.ts
@@ -19,6 +19,7 @@ export interface StudentGetAllResponse {
   studentNumber: string;
   citizenship?: string; // Državljanstvo
   birthMunicipality?: string; // Općina rođenja
+  isDeleted?: boolean;
 }
 
 @Injectable({
