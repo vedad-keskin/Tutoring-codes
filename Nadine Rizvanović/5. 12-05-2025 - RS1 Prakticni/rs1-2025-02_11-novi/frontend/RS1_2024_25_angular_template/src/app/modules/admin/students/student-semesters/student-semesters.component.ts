dcccdb2c678efa8e0c66bc8e214890ee81da91b3
Vedad Keskin <vedad.keskin98@gmail.com>
2025-05-13 12:24:00 +0200

    2A - Prikaz studenta - Nadine Rizvanovic

--- Nadine Rizvanović/5. 12-05-2025 - RS1 Prakticni/rs1-2025-02_11-novi/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
+++ Nadine Rizvanović/5. 12-05-2025 - RS1 Prakticni/rs1-2025-02_11-novi/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {
+  StudentGetByIdEndpointService, StudentGetByIdResponse
+} from '../../../../endpoints/student-endpoints/student-get-by-id-endpoint.service';
+import {MySnackbarHelperService} from '../../../shared/snackbars/my-snackbar-helper.service';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-student-semesters',
   standalone: false,
-  
+
   templateUrl: './student-semesters.component.html',
   styleUrl: './student-semesters.component.css'
 })
-export class StudentSemestersComponent {
+export class StudentSemestersComponent implements OnInit {
 
+  // Nase globalne varijable
+
+  studentId: number = 0;
+  student: any;
+  //student: StudentGetByIdResponse | null = null;
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private studentGetByIdService:StudentGetByIdEndpointService,
+    private snackbar: MySnackbarHelperService,
+    private router: Router,
+    private dialog: MatDialog,
+     ) {
+
+    this.studentId = this.route.snapshot.params['id'];
+
+  }
+
+  ngOnInit(): void {
+        this.fetchStudent();
+    }
+
+
+  private fetchStudent() {
+
+    this.studentGetByIdService.handleAsync(this.studentId).subscribe({
+      next: (data) => {
+
+        this.student = data;
+
+      },
+      error: (err) => {
+        this.snackbar.showMessage('Error restoring student. Please try again.', 5000);
+        console.error('Error deleting student:', err);
+      }
+    });
+
+  }
 }
