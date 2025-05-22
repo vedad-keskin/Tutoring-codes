4d22897592776d6c4453cc39470d82ef11a68a8f
Vedad Keskin <vedad.keskin98@gmail.com>
2025-05-22 20:21:54 +0200

    2B - Ajdin Alic

---
+++ Ajdin Alić/2. 17-06-2025 - RS1 Praktični T25/rs1-2025-02_11-novi - postavka/backend/RS1_2024_25.API/Endpoints/SemesterEndpoints/SemesterGetByStudentIdEndpoint.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RS1_2024_25.API.Data;
+using RS1_2024_25.API.Data.Models.SharedTables;
+using RS1_2024_25.API.Data.Models.TenantSpecificTables.Modul1_Auth;
+using RS1_2024_25.API.Helper.Api;
+using static RS1_2024_25.API.Endpoints.CityEndpoints.CityGetAll1Endpoint;
+using static RS1_2024_25.API.Endpoints.CityEndpoints.SemesterGetByStudentIdEndpoint;
+
+namespace RS1_2024_25.API.Endpoints.CityEndpoints;
+
+[Route("semesters")]
+public class SemesterGetByStudentIdEndpoint(ApplicationDbContext db) : MyEndpointBaseAsync
+    .WithRequest<int>
+    .WithResult<SemesterGetByStudentIdResponse[]>
+{
+    [HttpGet("{id}")]
+    public override async Task<SemesterGetByStudentIdResponse[]> HandleAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var result = await db.Semesters
+            .Where(x=> x.StudentId == id)
+            .Include(x=> x.AcademicYear)
+            .Include(x=> x.RecordedBy)
+                        .Select(c => new SemesterGetByStudentIdResponse
+                        {
+                            ID = c.ID,
+                            //                                        2023-2025
+                            AcademicYearName = $"{c.AcademicYear.StartDate.Year}-{c.AcademicYear.EndDate.Year}",
+                            StudyYear = c.StudyYear,
+                            Renewal = c.Renewal,
+                            DateOfEnrollment = c.DateOfEnrollment,
+                            RecordedByName = c.RecordedBy.FirstName
+                        })
+                        .ToArrayAsync(cancellationToken);
+
+        return result;
+    }
+
+    public class SemesterGetByStudentIdResponse
+    {
+        public required int ID { get; set; }
+        public string AcademicYearName { get; set; }
+        public int StudyYear { get; set; }
+        public bool Renewal { get; set; }
+        public DateTime DateOfEnrollment { get; set; }
+        public string RecordedByName { get; set; } 
+
+    }
+}
---
+++ Ajdin Alić/2. 17-06-2025 - RS1 Praktični T25/rs1-2025-02_11-novi - postavka/backend/RS1_2024_25.API/Endpoints/SemesterEndpoints/SemesterUpdateOrInsertEndpoint.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RS1_2024_25.API.Data;
+using RS1_2024_25.API.Data.Enums;
+using RS1_2024_25.API.Data.Models;
+using RS1_2024_25.API.Data.Models.SharedTables;
+using RS1_2024_25.API.Data.Models.TenantSpecificTables.Modul1_Auth;
+using RS1_2024_25.API.Data.Models.TenantSpecificTables.Modul2_Basic;
+using RS1_2024_25.API.Helper.Api;
+using RS1_2024_25.API.Services;
+using System.ComponentModel.DataAnnotations.Schema;
+using static RS1_2024_25.API.Endpoints.StudentEndpoints.SemesterUpdateOrInsertEndpoint;
+using static RS1_2024_25.API.Endpoints.StudentEndpoints.StudentUpdateOrInsertEndpoint;
+
+namespace RS1_2024_25.API.Endpoints.StudentEndpoints;
+
+[Route("semesters")]
+//[MyAuthorization(isAdmin: true, isManager: false)]
+public class SemesterUpdateOrInsertEndpoint(ApplicationDbContext db) : MyEndpointBaseAsync
+    .WithRequest<SemesterUpdateOrInsertRequest>
+    .WithActionResult<int>
+{
+    [HttpPost]  // Using POST to support both create and update
+    public override async Task<ActionResult<int>> HandleAsync(
+        [FromBody] SemesterUpdateOrInsertRequest request,
+        CancellationToken cancellationToken = default)
+    {
+  
+        // Check if it's an insert or update operation
+        bool isInsert = (request.ID == null || request.ID == 0);
+        Semester? semester;
+
+        if (isInsert)
+        {
+            semester = new Semester();
+
+            db.Add(semester);
+        }
+        else
+        {
+            // Update operation: retrieve the existing student
+            semester = await db.Semesters
+                .SingleOrDefaultAsync(x => x.ID == request.ID, cancellationToken);
+
+            if (semester == null)
+            {
+                return NotFound("Semester not found");
+            }
+        }
+
+        // Set common properties for both insert and update
+        semester.AcademicYearId = request.AcademicYearId;
+        semester.StudentId = request.StudentId;
+        semester.RecordedById = request.RecordedById;
+        semester.DateOfEnrollment = request.DateOfEnrollment;
+        semester.StudyYear = request.StudyYear;
+        semester.Price = request.Price;
+        semester.Renewal = request.Renewal;
+
+
+        // Save changes to the database
+        await db.SaveChangesAsync(cancellationToken);
+
+        return Ok(semester.ID); // Return the ID of the student
+    }
+
+    public class SemesterUpdateOrInsertRequest
+    {
+        public int? ID { get; set; } // Nullable to allow null for insert operations
+        public int AcademicYearId { get; set; } // FK na Akademska
+        public int StudentId { get; set; } // FK na student
+        public int RecordedById { get; set; } // FK na RecordedBy
+        public DateTime DateOfEnrollment { get; set; }
+        public int StudyYear { get; set; }
+        public float Price { get; set; }
+        public bool Renewal { get; set; }
+
+    }
+}
