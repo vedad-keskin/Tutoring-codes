e96cd1c6d2a7c479d7bbe1fa187d0cdb2797bb94
Vedad Keskin <vedad.keskin98@gmail.com>
2025-06-07 19:39:53 +0200

    2A - Navigacija - Ivan Radic

--- Ivan Radić/2. 07-06-2025 - RS1 Ispitni/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/students.component.ts
+++ Ivan Radić/2. 07-06-2025 - RS1 Ispitni/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/students.component.ts
@@ -134,13 +134,11 @@ export class StudentsComponent implements OnInit, AfterViewInit {
   }
 
   openStudentSemesters(id:number) {
-    this.dialog.open(MyDialogSimpleComponent, {
-      width: '350px',
-      data: {
-        title: 'Ispitni zadatak',
-        message: 'Implementirajte matičnu knjigu?'
-      }
-    });
+
+
+    this.router.navigate(['/admin/students/semesters', id]);
+
+
   }
 
   toggleDeleted() {
