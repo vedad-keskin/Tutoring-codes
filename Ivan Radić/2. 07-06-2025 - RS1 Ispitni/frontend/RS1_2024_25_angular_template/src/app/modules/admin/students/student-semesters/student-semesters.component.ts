38572ecdf4e1e1960e7d23b9d5b0a097c9743026
Vedad Keskin <vedad.keskin98@gmail.com>
2025-06-07 19:46:30 +0200

    2C - Prikaz studenta - Ivan Radic

--- Ivan Radić/2. 07-06-2025 - RS1 Ispitni/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
+++ Ivan Radić/2. 07-06-2025 - RS1 Ispitni/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {
+  StudentGetByIdEndpointService
+} from '../../../../endpoints/student-endpoints/student-get-by-id-endpoint.service';
+import {MySnackbarHelperService} from '../../../shared/snackbars/my-snackbar-helper.service';
 
 @Component({
   selector: 'app-student-semesters',
   standalone: false,
-  
+
   templateUrl: './student-semesters.component.html',
   styleUrl: './student-semesters.component.css'
 })
-export class StudentSemestersComponent {
+export class StudentSemestersComponent implements OnInit {
 
+  // Nase varijable
+
+  studentId:number= 0;
+  student:any;
+
+  constructor(    private route: ActivatedRoute,
+                  private router: Router,
+                  private studentGetByIdService:StudentGetByIdEndpointService,
+                  private snackbar: MySnackbarHelperService,
+
+  ) {
+
+    this.studentId = this.route.snapshot.params['id'];
+
+  }
+
+  ngOnInit(): void {
+
+    this.fetchStudent();
+
+    }
+
+
+  private fetchStudent() {
+
+    this.studentGetByIdService.handleAsync(this.studentId).subscribe({
+      next: (data) => {
+
+          this.student = data;
+
+      },
+      error: (err) => {
+        this.snackbar.showMessage('Error restoring student. Please try again.', 5000);
+        console.error('Error restoring student:', err);
+      }
+    });
+
+
+  }
 }
