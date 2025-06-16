a10772556225b6c0e5da0c86c4eadddba7e708da
Vedad Keskin <vedad.keskin98@gmail.com>
2025-06-16 20:11:56 +0200

    2A - Routing i ID iz rute - Dava

--- Dava/2. 16-06-2025 - T25 Ispitni/rs1-2025-02_11-novi - rjesenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
+++ Dava/2. 16-06-2025 - T25 Ispitni/rs1-2025-02_11-novi - rjesenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-student-semesters',
   standalone: false,
-  
+
   templateUrl: './student-semesters.component.html',
   styleUrl: './student-semesters.component.css'
 })
 export class StudentSemestersComponent {
+
+  studentId : number = 0;
+
+  constructor(   private route: ActivatedRoute,
+                 private router: Router,
+                 ) {
+
+    this.studentId = this.route.snapshot.params['id'];
+
+  }
 
 }
