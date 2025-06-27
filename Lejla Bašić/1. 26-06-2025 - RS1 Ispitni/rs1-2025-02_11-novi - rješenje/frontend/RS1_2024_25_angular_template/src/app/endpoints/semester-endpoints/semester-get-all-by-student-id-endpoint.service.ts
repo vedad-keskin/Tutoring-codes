516655010b497a8ae15e39ad65f19675b30e81c9
Vedad Keskin <vedad.keskin98@gmail.com>
2025-06-27 16:13:15 +0200

    2B - Frontend Endpoints - Lejla Basic

---
+++ Lejla Bašić/1. 26-06-2025 - RS1 Ispitni/rs1-2025-02_11-novi - rješenje/frontend/RS1_2024_25_angular_template/src/app/endpoints/semester-endpoints/semester-get-all-by-student-id-endpoint.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MyConfig } from '../../my-config';
+import { MyBaseEndpointAsync } from '../../helper/my-base-endpoint-async.interface';
+
+export interface SemesterGetAllByStudentIdResponse {
+  id: number;
+  academicYearName:string;
+  studyYear:number;
+  renewal:boolean;
+  dateOfEnrollemnt:Date;
+  recordedByName:string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SemesterGetAllByStudentIdEndpoint
+  implements MyBaseEndpointAsync<number, SemesterGetAllByStudentIdResponse> {
+  private apiUrl = `${MyConfig.api_address}/semesters`;
+
+  constructor(private httpClient: HttpClient) {}
+
+  handleAsync(id: number) {
+    return this.httpClient.get<SemesterGetAllByStudentIdResponse>(`${this.apiUrl}/${id}`);
+  }
+}
