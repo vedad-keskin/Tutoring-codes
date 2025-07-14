38efbe41e07c4d388ab59f424c49aa8a01854957
Vedad Keskin <vedad.keskin98@gmail.com>
2025-07-14 11:41:18 +0200

    1A - Adna

--- Adna Hadžihasanović/14-07-2025 - RS1 Praktični/rs1-2025-02_11-novi - rješenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/students.component.ts
+++ Adna Hadžihasanović/14-07-2025 - RS1 Praktični/rs1-2025-02_11-novi - rješenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/students.component.ts
@@ -25,6 +25,12 @@ export class StudentsComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
+  // Nase varijable
+
+  showDeleted: boolean = false;
+
+
+
   constructor(
     private studentGetService: StudentGetAllEndpointService,
     private studentDeleteService: StudentDeleteEndpointService,
@@ -56,7 +62,11 @@ export class StudentsComponent implements OnInit, AfterViewInit {
       pageSize: pageSize
     }).subscribe({
       next: (data) => {
-        this.dataSource = new MatTableDataSource<StudentGetAllResponse>(data.dataItems);
+        this.dataSource = new MatTableDataSource<StudentGetAllResponse>(
+
+          this.showDeleted ? data.dataItems : data.dataItems.filter((x:any)=> !x.isDeleted )
+
+        );
         this.paginator.length = data.totalCount;
       },
       error: (err) => {
@@ -110,5 +120,13 @@ export class StudentsComponent implements OnInit, AfterViewInit {
         message: 'Implementirajte matičnu knjigu?'
       }
     });
+  }
+
+  toggleDeleted() {
+
+      this.showDeleted = !this.showDeleted;
+
+      this.fetchStudents();
+
   }
 }
