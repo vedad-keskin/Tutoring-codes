20de614e1f79acc6c54c6a1b1d68468483a92573
Vedad Keskin <vedad.keskin98@gmail.com>
2025-07-15 15:49:52 +0200

    Add initial RS1 practical exam project structure
    Initial commit for the RS1 practical exam project, including backend (ASP.NET Core API with models, endpoints, services, migrations, and configuration) and frontend (Angular template with modules, components, services, and assets). Also includes helper utilities, authentication, and city/academic year/semester/student management features. Removes outdated exam notes PDF.

---
+++ Nadine Rizvanović/6. 15-07-2025 - RS1 Prakticni FEB25/rs1-2025-02_11-novi/backend/RS1_2024_25.API/Endpoints/AcademicYearEndpoints/AcademicYearGetAllEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RS1_2024_25.API.Data;
+using RS1_2024_25.API.Helper.Api;
+using static RS1_2024_25.API.Endpoints.AcademicYearEndpoints.AcademicYearGetAllEndpoint;
+
+namespace RS1_2024_25.API.Endpoints.AcademicYearEndpoints;
+
+[Route("academicYear")]
+public class AcademicYearGetAllEndpoint(ApplicationDbContext db) : MyEndpointBaseAsync
+    .WithoutRequest
+    .WithResult<AcademicYearGetAllResponse[]>
+{
+    [HttpGet("all")]
+    public override async Task<AcademicYearGetAllResponse[]> HandleAsync(CancellationToken cancellationToken = default)
+    {
+        var result = await db.AcademicYears
+                        .Select(c => new AcademicYearGetAllResponse
+                        {
+                            ID = c.ID,
+                            Name = c.Description,
+                        })
+                        .ToArrayAsync(cancellationToken);
+
+        return result;
+    }
+
+    public class AcademicYearGetAllResponse
+    {
+        public required int ID { get; set; }
+        public required string Name { get; set; }
+    }
+}
---
+++ Nadine Rizvanović/6. 15-07-2025 - RS1 Prakticni FEB25/rs1-2025-02_11-novi/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
@@ -0,0 +1,88 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {
+  StudentGetByIdEndpointService, StudentGetByIdResponse
+} from '../../../../endpoints/student-endpoints/student-get-by-id-endpoint.service';
+import {MySnackbarHelperService} from '../../../shared/snackbars/my-snackbar-helper.service';
+import {MatDialog} from '@angular/material/dialog';
+import {
+  SemesterGetAllByStudentIdEndpoint
+} from '../../../../endpoints/semester-endpoints/semester-get-all-by-student-id-endpoint.service';
+
+@Component({
+  selector: 'app-student-semesters',
+  standalone: false,
+
+  templateUrl: './student-semesters.component.html',
+  styleUrl: './student-semesters.component.css'
+})
+export class StudentSemestersComponent implements OnInit {
+
+  // Nase globalne varijable
+
+  studentId: number = 0;
+  //student: any;
+  student: StudentGetByIdResponse | null = null;
+  semesters:any;
+
+  displayedColumns: string[] = ['id', 'academicYear', 'yearOfStudy', 'renewal', 'winterSemester', 'recordedBy'];
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private studentGetByIdService:StudentGetByIdEndpointService,
+    private snackbar: MySnackbarHelperService,
+    private router: Router,
+    private dialog: MatDialog,
+    private semesterGetAllByStudentIdEndpoint:SemesterGetAllByStudentIdEndpoint
+     ) {
+
+    this.studentId = this.route.snapshot.params['id'];
+
+  }
+
+  ngOnInit(): void {
+        this.fetchStudent();
+        this.fetchSemesters();
+    }
+
+
+  private fetchStudent() {
+
+    this.studentGetByIdService.handleAsync(this.studentId).subscribe({
+      next: (data) => {
+
+        this.student = data;
+
+      },
+      error: (err) => {
+        this.snackbar.showMessage('Error fetching student. Please try again.', 5000);
+        console.error('Error fetching student:', err);
+      }
+    });
+
+  }
+
+  private fetchSemesters() {
+
+    this.semesterGetAllByStudentIdEndpoint.handleAsync(this.studentId).subscribe({
+      next: (data) => {
+
+        this.semesters = data;
+
+      },
+      error: (err) => {
+        this.snackbar.showMessage('Error fetching semesters. Please try again.', 5000);
+        console.error('Error fetching semesters:', err);
+      }
+    });
+
+
+  }
+
+  openNewSemestersPage() {
+
+    this.router.navigate(['/admin/students/semesters/new', this.studentId]);
+
+  }
+}
