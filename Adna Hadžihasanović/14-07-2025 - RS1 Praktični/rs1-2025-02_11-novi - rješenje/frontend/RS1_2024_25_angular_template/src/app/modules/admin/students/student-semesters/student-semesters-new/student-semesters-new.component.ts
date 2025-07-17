0dd77bec98aa20e23f9baec96eb7db4d05222e24
Vedad Keskin <vedad.keskin98@gmail.com>
2025-07-17 11:43:19 +0200

    Full dodavanje novog semestra - Adna

--- Adna Hadžihasanović/14-07-2025 - RS1 Praktični/rs1-2025-02_11-novi - rješenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters-new/student-semesters-new.component.ts
+++ Adna Hadžihasanović/14-07-2025 - RS1 Praktični/rs1-2025-02_11-novi - rješenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters-new/student-semesters-new.component.ts
@@ -1,12 +1,148 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {
+  StudentGetByIdEndpointService, StudentGetByIdResponse
+} from '../../../../../endpoints/student-endpoints/student-get-by-id-endpoint.service';
+import {MySnackbarHelperService} from '../../../../shared/snackbars/my-snackbar-helper.service';
+import {MatDialog} from '@angular/material/dialog';
+import {
+  SemesterGetAllByStudentIdEndpoint
+} from '../../../../../endpoints/semester-endpoints/semester-get-all-by-student-id-endpoint.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {
+  AcademicYearGetAllEndpoint
+} from '../../../../../endpoints/academic-year-endpoints/academic-year-get-all-endpoint.service';
+import {
+  StudentUpdateOrInsertRequest
+} from '../../../../../endpoints/student-endpoints/student-update-or-insert-endpoint.service';
+import {
+  SemesterUpdateOrInsertEndpoint,
+  SemesterUpdateOrInsertRequest
+} from '../../../../../endpoints/semester-endpoints/semester-update-or-insert-endpoint.service';
 
 @Component({
   selector: 'app-student-semesters-new',
   standalone: false,
-  
+
   templateUrl: './student-semesters-new.component.html',
   styleUrl: './student-semesters-new.component.css'
 })
-export class StudentSemestersNewComponent {
+export class StudentSemestersNewComponent implements OnInit  {
 
+  // Nase varijable
+  studentId: number = 0;
+  //student:any;
+  student: StudentGetByIdResponse | null = null;
+  semesters:any;
+  semesterForm: FormGroup;
+  academicYears:any;
+
+  constructor(    private route: ActivatedRoute,
+                  private router: Router,
+                  private studentGetByIdService:StudentGetByIdEndpointService,
+                  private snackbar: MySnackbarHelperService,
+                  private dialog: MatDialog,
+                  private semesterGetAllByStudentIdService:SemesterGetAllByStudentIdEndpoint,
+                  private fb: FormBuilder,
+                  private academicYearGetAllService:AcademicYearGetAllEndpoint,
+                  private semesterUpdateOrInsertService:SemesterUpdateOrInsertEndpoint
+  ) {
+
+    this.studentId = this.route.snapshot.params['id'];
+
+    this.semesterForm = this.fb.group({
+
+      academicYearId: [1, [Validators.required]],
+      studentId: [this.studentId, [Validators.required]],
+      recordedById: [1, [Validators.required]],
+      dateOfEnrollment: [new Date(), [Validators.required]],
+      yearOfStudy: [null, [Validators.required]],
+      price: [null, [Validators.required, Validators.max(2000), Validators.min(50) ]],
+      renewal: [false, [Validators.required]],
+
+    });
+
+  }
+
+  ngOnInit(): void {
+
+    this.fetchStudent();
+    this.fetchSemesters();
+    this.fetchAcademicYears();
+
+  }
+
+  private fetchStudent() {
+
+    this.studentGetByIdService.handleAsync(this.studentId).subscribe({
+      next: (data) => {
+
+        this.student = data;
+
+
+      },
+      error: (err) => {
+        this.snackbar.showMessage('Error fetching student. Please try again.', 5000);
+        console.error('Error fetching student:', err);
+      }
+    });
+
+
+  }
+
+  private fetchSemesters() {
+
+    this.semesterGetAllByStudentIdService.handleAsync(this.studentId).subscribe({
+      next: (data) => {
+
+        this.semesters = data;
+
+
+      },
+      error: (err) => {
+        this.snackbar.showMessage('Error fetching semesters. Please try again.', 5000);
+        console.error('Error fetching semesters:', err);
+      }
+    });
+
+
+  }
+
+  saveSemester() {
+
+    if (this.semesterForm.invalid) return;
+
+    const semesterData: SemesterUpdateOrInsertRequest = {
+      ...this.semesterForm.value,
+    };
+
+    this.semesterUpdateOrInsertService.handleAsync(semesterData).subscribe({
+      next: () => {
+
+        this.router.navigate(['/admin/students/semesters', this.studentId]);
+      },
+      error: (error) => {
+
+        console.error('Error saving semester', error);
+      },
+    });
+
+  }
+
+  private fetchAcademicYears() {
+
+    this.academicYearGetAllService.handleAsync().subscribe({
+      next: (data) => {
+
+        this.academicYears = data;
+
+
+      },
+      error: (err) => {
+        this.snackbar.showMessage('Error fetching academic years. Please try again.', 5000);
+        console.error('Error fetching academic years:', err);
+      }
+    });
+
+  }
 }
