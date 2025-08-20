38825ac09dd614b07fd88b50cbdf5754a1aab1be
Lejla Bašić <vedad.keskin98@gmail.com>
2025-08-20 16:21:30 +0200

    2C - Prikaz imena - Tara Prasko

--- Tara Prasko/1. 20-08-2025 - RSI Ispitni/rs1-2025-02_11-novi - rjesenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
+++ Tara Prasko/1. 20-08-2025 - RSI Ispitni/rs1-2025-02_11-novi - rjesenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
@@ -1,7 +1,7 @@
-import {Component, Inject} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {
-  StudentGetByIdEndpointService
+  StudentGetByIdEndpointService, StudentGetByIdResponse
 } from '../../../../endpoints/student-endpoints/student-get-by-id-endpoint.service';
 import {
   StudentUpdateOrInsertEndpointService
@@ -10,6 +10,7 @@ import {
   MunicipalityLookupEndpointService
 } from '../../../../endpoints/lookup-endpoints/municipality-lookup-endpoint.service';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import {MySnackbarHelperService} from '../../../shared/snackbars/my-snackbar-helper.service';
 
 @Component({
   selector: 'app-student-semesters',
@@ -18,21 +19,46 @@ import {MatSnackBar} from '@angular/material/snack-bar';
   templateUrl: './student-semesters.component.html',
   styleUrl: './student-semesters.component.css'
 })
-export class StudentSemestersComponent {
+export class StudentSemestersComponent implements OnInit {
 
   studentId: number = 0;
+  //student:any;
+  student : StudentGetByIdResponse | null = null;
+
 
 
   constructor(    private route: ActivatedRoute,
                   private router: Router,
-                  private snackBar: MatSnackBar
+                  private snackbar: MySnackbarHelperService,
+                  private studentGetByIdService:StudentGetByIdEndpointService
   )
   {
 
     this.studentId = route.snapshot.params['id'];
 
-
   }
 
+  ngOnInit(): void {
 
+    this.fetchStudent();
+
+    }
+
+
+  private fetchStudent() {
+
+    this.studentGetByIdService.handleAsync(this.studentId).subscribe({
+      next: (data) => {
+        this.snackbar.showMessage('Student successfully fetched.');
+
+        this.student = data;
+
+      },
+      error: (err) => {
+        this.snackbar.showMessage('Error fetching student. Please try again.', 5000);
+        console.error('Error fetching student:', err);
+      }
+    });
+
+  }
 }
