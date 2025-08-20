5254cc6465ca2fff7b7cea82ce5e2093e471f293
Lejla Bašić <vedad.keskin98@gmail.com>
2025-08-20 16:08:45 +0200

    2A - Tara Prasko

--- Tara Prasko/1. 20-08-2025 - RSI Ispitni/rs1-2025-02_11-novi - rjesenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
+++ Tara Prasko/1. 20-08-2025 - RSI Ispitni/rs1-2025-02_11-novi - rjesenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {
+  StudentGetByIdEndpointService
+} from '../../../../endpoints/student-endpoints/student-get-by-id-endpoint.service';
+import {
+  StudentUpdateOrInsertEndpointService
+} from '../../../../endpoints/student-endpoints/student-update-or-insert-endpoint.service';
+import {
+  MunicipalityLookupEndpointService
+} from '../../../../endpoints/lookup-endpoints/municipality-lookup-endpoint.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-student-semesters',
   standalone: false,
-  
+
   templateUrl: './student-semesters.component.html',
   styleUrl: './student-semesters.component.css'
 })
 export class StudentSemestersComponent {
+
+  studentId: number = 0;
+
+
+  constructor(    private route: ActivatedRoute,
+                  private router: Router,
+                  private snackBar: MatSnackBar
+  )
+  {
+
+    this.studentId = route.snapshot.params['id'];
+
+
+  }
+
 
 }
