e61a3f0c0147bcc53f3fb1b5d13e18c10bdf72c3
Lejla Bašić <vedad.keskin98@gmail.com>
2025-08-23 13:16:26 +0200

    Semester tabela - Tara Prasko

---
+++ Tara Prasko/1. 20-08-2025 - RSI Ispitni/rs1-2025-02_11-novi - rjesenje/backend/RS1_2024_25.API/Data/Models/Semester.cs
@@ -0,0 +1,41 @@
+﻿using RS1_2024_25.API.Data.Models.SharedTables;
+using RS1_2024_25.API.Data.Models.TenantSpecificTables.Modul1_Auth;
+using RS1_2024_25.API.Data.Models.TenantSpecificTables.Modul2_Basic;
+using RS1_2024_25.API.Helper.BaseClasses;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RS1_2024_25.API.Data.Models
+{
+    public class Semester : BaseEntity
+    {
+        //• akademska godina: FK na tabelu AkademskaGodina(prikazati unutar combobox-a)
+
+        public int AcademicYearId { get; set; }
+        [ForeignKey(nameof(AcademicYearId))]
+        public AcademicYear? AcademicYear { get; set; } 
+
+        // FK na Student
+
+        public int StudentId { get; set; } 
+        [ForeignKey(nameof(StudentId))]
+        public Student? Student { get; set; }
+
+        // FK na RecordedBy
+
+        public int RecordedById { get; set; }
+        [ForeignKey(nameof(RecordedById))]
+        public MyAppUser? RecordedBy { get; set; }
+
+        //• datum upisa: DateTime
+        //• godina studija: int
+        //• cijena školarine: float 
+        //• obnova: bool
+
+        public DateTime DateOfEnrollment { get; set; }
+        public int YearOfStudy { get; set; }
+        public float Price { get; set; }
+        public bool Renewal { get; set; }
+
+
+    }
+}
