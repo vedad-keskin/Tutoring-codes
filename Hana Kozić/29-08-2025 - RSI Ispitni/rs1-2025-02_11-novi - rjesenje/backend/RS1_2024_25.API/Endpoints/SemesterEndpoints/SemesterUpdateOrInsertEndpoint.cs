0bba9d4ee7bb81d3c60a6d2ca7acf565f5008c84
Vedad Keskin <vedad.keskin98@gmail.com>
2025-09-02 20:20:30 +0200

    2B - Kreiranje klase i endpointi na backendu - Hana Kozic

---
+++ Hana Kozić/29-08-2025 - RSI Ispitni/rs1-2025-02_11-novi - rjesenje/backend/RS1_2024_25.API/Endpoints/SemesterEndpoints/SemesterUpdateOrInsertEndpoint.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RS1_2024_25.API.Data;
+using RS1_2024_25.API.Data.Models;
+using RS1_2024_25.API.Data.Models.SharedTables;
+using RS1_2024_25.API.Data.Models.TenantSpecificTables.Modul1_Auth;
+using RS1_2024_25.API.Data.Models.TenantSpecificTables.Modul2_Basic;
+using RS1_2024_25.API.Helper.Api;
+using RS1_2024_25.API.Services;
+using System.ComponentModel.DataAnnotations.Schema;
+using static RS1_2024_25.API.Endpoints.CityEndpoints.SemesterUpdateOrInsertEndpoint;
+
+namespace RS1_2024_25.API.Endpoints.CityEndpoints;
+
+[Route("semesters")]
+//[MyAuthorization(isAdmin: true, isManager: false)]
+public class SemesterUpdateOrInsertEndpoint(ApplicationDbContext db) : MyEndpointBaseAsync
+    .WithRequest<SemesterUpdateOrInsertRequest>
+    .WithActionResult<int>
+{
+    [HttpPost]  // Using POST to support both create and update
+    public override async Task<ActionResult<int>> HandleAsync([FromBody] SemesterUpdateOrInsertRequest request, CancellationToken cancellationToken = default)
+    {
+
+        // Check if we're performing an insert or update based on the ID value
+        bool isInsert = (request.ID == null || request.ID == 0);
+        Semester? semester;
+
+        if (isInsert)
+        {
+            // Insert operation: create a new city entity
+            semester = new Semester();
+            db.Semesters.Add(semester); // Add the new city to the context
+        }
+        else
+        {
+            // Update operation: retrieve the existing city
+            semester = await db.Semesters.SingleOrDefaultAsync(x => x.ID == request.ID, cancellationToken);
+
+            if (semester == null)
+            {
+                return NotFound("Semester not found");
+            }
+        }
+
+        // Set common properties for both insert and update operations
+        semester.StudentId = request.StudentId;
+        semester.AcademicYearId = request.AcademicYearId;
+        semester.RecordedById = request.RecordedById;
+        semester.YearOfStudy = request.YearOfStudy;
+        semester.DateOfEnrollment = request.DateOfEnrollment;
+        semester.Renewal = request.Renewal;
+        semester.Price = request.Price;
+
+        // Save changes to the database
+        await db.SaveChangesAsync(cancellationToken);
+
+        return Ok(semester.ID);
+    }
+
+    public class SemesterUpdateOrInsertRequest
+    {
+        public int? ID { get; set; } // Nullable to allow null for insert operations
+
+        public int AcademicYearId { get; set; }
+        public int StudentId { get; set; }
+        public int RecordedById { get; set; }
+
+        public DateTime DateOfEnrollment { get; set; }
+        public int YearOfStudy { get; set; }
+        public float Price { get; set; }
+        public bool Renewal { get; set; }
+
+
+
+    }
+}
--- Hana Kozić/29-08-2025 - RSI Ispitni/rs1-2025-02_11-novi - rjesenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
+++ Hana Kozić/29-08-2025 - RSI Ispitni/rs1-2025-02_11-novi - rjesenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {
+  StudentGetByIdEndpointService, StudentGetByIdResponse
+} from '../../../../endpoints/student-endpoints/student-get-by-id-endpoint.service';
+import {MySnackbarHelperService} from '../../../shared/snackbars/my-snackbar-helper.service';
 
 @Component({
   selector: 'app-student-semesters',
   standalone: false,
-  
+
   templateUrl: './student-semesters.component.html',
   styleUrl: './student-semesters.component.css'
 })
-export class StudentSemestersComponent {
+export class StudentSemestersComponent implements OnInit {
 
+  // Nase varijable
+  studentId:number = 0;
+  //student:any;
+  student: StudentGetByIdResponse | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private snackbar: MySnackbarHelperService,
+    private studentGetByIdService:StudentGetByIdEndpointService
+  ) {
+
+    this.studentId = route.snapshot.params['id'];
+
+  }
+
+  ngOnInit(): void {
+
+    this.fetchStudent();
+
+    }
+
+
+  private fetchStudent() {
+
+    this.studentGetByIdService.handleAsync(this.studentId).subscribe({
+      next: (data) => {
+
+          this.student = data;
+
+      },
+      error: (err) => {
+        this.snackbar.showMessage('Error fetching student. Please try again.', 5000);
+        console.error('Error fetching student:', err);
+      }
+    });
+
+  }
 }
