c176754cfdc84afeefaca7c10f453b23faa45b1e
Vedad Keskin <vedad.keskin98@gmail.com>
2025-09-09 02:03:41 +0200

    09-08-2025 - Stipendije Dalila Semic

---
+++ Dalila Šemić/09-08-2025 - PRIII Stipendije/Template 2025/DLWMS.WinApp/IspitIB180079/frmPretragaIB180079.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace DLWMS.WinApp.IspitIB180079
+{
+    partial class frmPretragaIB180079
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            cbGodina = new ComboBox();
+            cbStipendija = new ComboBox();
+            btnDodaj = new Button();
+            btnStipendije = new Button();
+            dgvStudentiStipendije = new DataGridView();
+            Student = new DataGridViewTextBoxColumn();
+            GodinaInfo = new DataGridViewTextBoxColumn();
+            StipendijaInfo = new DataGridViewTextBoxColumn();
+            IznosInfo = new DataGridViewTextBoxColumn();
+            UkupnoInfo = new DataGridViewTextBoxColumn();
+            Ukloni = new DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)dgvStudentiStipendije).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Godina:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(261, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 20);
+            label2.TabIndex = 0;
+            label2.Text = "Stipendija:";
+            // 
+            // cbGodina
+            // 
+            cbGodina.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbGodina.FormattingEnabled = true;
+            cbGodina.Items.AddRange(new object[] { "2025", "2024", "2023", "2022", "2021" });
+            cbGodina.Location = new Point(12, 32);
+            cbGodina.Name = "cbGodina";
+            cbGodina.Size = new Size(244, 28);
+            cbGodina.TabIndex = 1;
+            cbGodina.SelectedIndexChanged += cbGodina_SelectedIndexChanged;
+            // 
+            // cbStipendija
+            // 
+            cbStipendija.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbStipendija.FormattingEnabled = true;
+            cbStipendija.Location = new Point(262, 32);
+            cbStipendija.Name = "cbStipendija";
+            cbStipendija.Size = new Size(244, 28);
+            cbStipendija.TabIndex = 1;
+            cbStipendija.SelectedIndexChanged += cbStipendija_SelectedIndexChanged;
+            // 
+            // btnDodaj
+            // 
+            btnDodaj.Location = new Point(683, 31);
+            btnDodaj.Name = "btnDodaj";
+            btnDodaj.Size = new Size(171, 29);
+            btnDodaj.TabIndex = 2;
+            btnDodaj.Text = "Dodaj stipendiju";
+            btnDodaj.UseVisualStyleBackColor = true;
+            btnDodaj.Click += btnDodaj_Click;
+            // 
+            // btnStipendije
+            // 
+            btnStipendije.Location = new Point(860, 31);
+            btnStipendije.Name = "btnStipendije";
+            btnStipendije.Size = new Size(208, 29);
+            btnStipendije.TabIndex = 2;
+            btnStipendije.Text = "Stipendije po godinama";
+            btnStipendije.UseVisualStyleBackColor = true;
+            // 
+            // dgvStudentiStipendije
+            // 
+            dgvStudentiStipendije.AllowUserToAddRows = false;
+            dgvStudentiStipendije.AllowUserToDeleteRows = false;
+            dgvStudentiStipendije.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvStudentiStipendije.Columns.AddRange(new DataGridViewColumn[] { Student, GodinaInfo, StipendijaInfo, IznosInfo, UkupnoInfo, Ukloni });
+            dgvStudentiStipendije.Location = new Point(12, 66);
+            dgvStudentiStipendije.Name = "dgvStudentiStipendije";
+            dgvStudentiStipendije.ReadOnly = true;
+            dgvStudentiStipendije.RowHeadersWidth = 51;
+            dgvStudentiStipendije.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvStudentiStipendije.Size = new Size(1056, 345);
+            dgvStudentiStipendije.TabIndex = 3;
+            dgvStudentiStipendije.CellContentClick += dgvStudentiStipendije_CellContentClick;
+            // 
+            // Student
+            // 
+            Student.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Student.DataPropertyName = "Student";
+            Student.HeaderText = "(Indeks) Ime i prezime";
+            Student.MinimumWidth = 6;
+            Student.Name = "Student";
+            Student.ReadOnly = true;
+            // 
+            // GodinaInfo
+            // 
+            GodinaInfo.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            GodinaInfo.DataPropertyName = "GodinaInfo";
+            GodinaInfo.HeaderText = "Godina";
+            GodinaInfo.MinimumWidth = 6;
+            GodinaInfo.Name = "GodinaInfo";
+            GodinaInfo.ReadOnly = true;
+            // 
+            // StipendijaInfo
+            // 
+            StipendijaInfo.DataPropertyName = "StipendijaInfo";
+            StipendijaInfo.HeaderText = "Stipendija";
+            StipendijaInfo.MinimumWidth = 6;
+            StipendijaInfo.Name = "StipendijaInfo";
+            StipendijaInfo.ReadOnly = true;
+            StipendijaInfo.Width = 125;
+            // 
+            // IznosInfo
+            // 
+            IznosInfo.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            IznosInfo.DataPropertyName = "IznosInfo";
+            IznosInfo.HeaderText = "Mjesecni iznos";
+            IznosInfo.MinimumWidth = 6;
+            IznosInfo.Name = "IznosInfo";
+            IznosInfo.ReadOnly = true;
+            // 
+            // UkupnoInfo
+            // 
+            UkupnoInfo.DataPropertyName = "UkupnoInfo";
+            UkupnoInfo.HeaderText = "Ukupno";
+            UkupnoInfo.MinimumWidth = 6;
+            UkupnoInfo.Name = "UkupnoInfo";
+            UkupnoInfo.ReadOnly = true;
+            UkupnoInfo.Width = 125;
+            // 
+            // Ukloni
+            // 
+            Ukloni.HeaderText = "";
+            Ukloni.MinimumWidth = 6;
+            Ukloni.Name = "Ukloni";
+            Ukloni.ReadOnly = true;
+            Ukloni.Text = "Ukloni";
+            Ukloni.UseColumnTextForButtonValue = true;
+            Ukloni.Width = 125;
+            // 
+            // frmPretragaIB180079
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1080, 423);
+            Controls.Add(dgvStudentiStipendije);
+            Controls.Add(btnStipendije);
+            Controls.Add(btnDodaj);
+            Controls.Add(cbStipendija);
+            Controls.Add(cbGodina);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "frmPretragaIB180079";
+            Text = "Broj prikazanin studenata: placeholder";
+            Load += frmPretragaIB180079_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvStudentiStipendije).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private ComboBox cbGodina;
+        private ComboBox cbStipendija;
+        private Button btnDodaj;
+        private Button btnStipendije;
+        private DataGridView dgvStudentiStipendije;
+        private DataGridViewTextBoxColumn Student;
+        private DataGridViewTextBoxColumn GodinaInfo;
+        private DataGridViewTextBoxColumn StipendijaInfo;
+        private DataGridViewTextBoxColumn IznosInfo;
+        private DataGridViewTextBoxColumn UkupnoInfo;
+        private DataGridViewButtonColumn Ukloni;
+    }
+}
---
+++ Dalila Šemić/09-08-2025 - PRIII Stipendije/Template 2025/DLWMS.WinApp/IspitIB180079/frmPretragaIB180079.cs
@@ -0,0 +1,121 @@
+﻿using DLWMS.Data.IspitIB180079;
+using DLWMS.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DLWMS.WinApp.IspitIB180079
+{
+    public partial class frmPretragaIB180079 : Form
+    {
+        DLWMSContext db = new DLWMSContext();
+        public frmPretragaIB180079()
+        {
+            InitializeComponent();
+        }
+
+        private void cbGodina_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UcitajStudentiStipendije();
+        }
+
+        private void UcitajStudentiStipendije()
+        {
+
+            var godina = cbGodina?.SelectedItem?.ToString() ?? ""; // "2025" , "2024"
+
+            // Id = 0 , Naziv = "" .. 
+            var stipendija = cbStipendija?.SelectedItem as StipendijeIB180079 ?? new StipendijeIB180079();
+
+            var studentStipendije = db.StudentiStipendijeIB180079
+                .Include(x => x.Student)
+                .Include(x => x.StipendijaGodina.Stipendija)
+                .Where(x => x.StipendijaGodina.Godina == godina)
+                .Where(x => x.StipendijaGodina.StipendijaId == stipendija.Id)
+                .ToList();
+
+            if (studentStipendije != null)
+            {
+
+                dgvStudentiStipendije.DataSource = null;
+                dgvStudentiStipendije.DataSource = studentStipendije;
+
+            }
+
+            Text = $"Broj prikazanih studenata: {studentStipendije.Count()}";
+
+            if (studentStipendije.Count() == 0 && stipendija.Id != 0)
+            {
+
+                MessageBox.Show($"U bazi nisu evidentirani studenti kojima je u {godina}. godini dodijeljena {stipendija} stipendija.", "Upozorenje", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+
+        }
+
+        private void frmPretragaIB180079_Load(object sender, EventArgs e)
+        {
+            dgvStudentiStipendije.AutoGenerateColumns = false;
+
+            UcitajComboBox();
+        }
+
+        private void UcitajComboBox()
+        {
+            cbGodina.SelectedIndex = 0;
+
+            cbStipendija.DataSource = db.StipendijeIB180079.ToList();
+        }
+
+        private void cbStipendija_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UcitajStudentiStipendije();
+        }
+
+        private void dgvStudentiStipendije_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if (e.ColumnIndex == 5)
+            {
+
+
+
+                if (MessageBox.Show("Da li ste sigurni da želite izbrisati odabranu stipendiju ? ", "Pitanje", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+                {
+
+                    var odabranaStudentStipendija = dgvStudentiStipendije.SelectedRows[0].DataBoundItem as StudentiStipendijeIB180079;
+
+                    db.StudentiStipendijeIB180079.Remove(odabranaStudentStipendija);
+                    db.SaveChanges();
+
+                    UcitajStudentiStipendije();
+
+
+
+                }
+
+
+            }
+
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+
+            var frmAddStipendija = new frmStipendijaAddEditIB180079();
+
+            if (frmAddStipendija.ShowDialog() == DialogResult.OK)
+            {
+                UcitajStudentiStipendije();
+            }
+
+        }
+    }
+}
