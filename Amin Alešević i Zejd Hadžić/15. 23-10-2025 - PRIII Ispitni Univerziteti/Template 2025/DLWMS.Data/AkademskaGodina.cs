67b9438a2227c9cefa11c148a46bbde57ba81b36
Vedad Keskin <vedad.keskin98@gmail.com>
2025-10-23 18:07:15 +0200

    23-10-2025 - PRIII Univ Amin i Zejd

---
+++ Amin Alešević i Zejd Hadžić/15. 23-10-2025 - PRIII Ispitni Univerziteti/Template 2025/DLWMS.Data/AkademskaGodina.cs
@@ -0,0 +1,18 @@
+﻿namespace DLWMS.Data
+{
+    public class AkademskaGodina
+    {
+        public int Id { get; set; } // PK 
+        public string Naziv { get; set; }
+        public string Opis { get; set; }
+        public bool Aktivan { get; set; }
+
+
+        // friend ostream& ... 
+        public override string ToString()
+        {
+            // COUT << Naziv ;
+            return Naziv;
+        }
+    }
+}
---
+++ Amin Alešević i Zejd Hadžić/15. 23-10-2025 - PRIII Ispitni Univerziteti/Template 2025/DLWMS.Data/Grad.cs
@@ -0,0 +1,23 @@
+﻿namespace DLWMS.Data
+{
+    public class Grad
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public string Oznaka { get; set; }
+
+        // FK -> strani kljuc 
+        public int DrzavaId { get; set; } // 1/2 .. 
+        public Drzava Drzava { get; set; } // BiH true .. 
+
+
+        public bool Aktivan { get; set; }
+
+
+        public override string ToString()
+        {
+            return Naziv;
+        }
+
+    }
+}
