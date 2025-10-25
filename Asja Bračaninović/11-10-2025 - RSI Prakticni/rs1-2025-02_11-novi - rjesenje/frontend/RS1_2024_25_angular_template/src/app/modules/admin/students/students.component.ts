66ce563d819e5efe309e523085bef57c362d4059
Vedad Keskin <vedad.keskin98@gmail.com>
2025-10-25 13:57:05 +0200

    1C - Asja B.

--- Asja Bračaninović/11-10-2025 - RSI Prakticni/rs1-2025-02_11-novi - rjesenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/students.component.ts
+++ Asja Bračaninović/11-10-2025 - RSI Prakticni/rs1-2025-02_11-novi - rjesenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/students.component.ts
@@ -7,11 +7,12 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
+import {debounceTime, distinctUntilChanged, filter, Subject} from 'rxjs';
 import { MyDialogConfirmComponent } from '../../shared/dialogs/my-dialog-confirm/my-dialog-confirm.component';
 import {MySnackbarHelperService} from '../../shared/snackbars/my-snackbar-helper.service';
 import {MyDialogSimpleComponent} from '../../shared/dialogs/my-dialog-simple/my-dialog-simple.component';
 import {StudentRestoreEndpointService} from '../../../endpoints/student-endpoints/student-restore-endpoint.service';
+import {map, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-students',
@@ -32,6 +33,7 @@ export class StudentsComponent implements OnInit, AfterViewInit {
   // Nase varijable
 
   showDeleted: boolean = false;
+  private searchSubject: Subject<string> = new Subject();
 
 
 
@@ -45,6 +47,7 @@ export class StudentsComponent implements OnInit, AfterViewInit {
   ) {}
 
   ngOnInit(): void {
+    this.initSearchListener();
     this.fetchStudents();
   }
 
@@ -55,9 +58,26 @@ export class StudentsComponent implements OnInit, AfterViewInit {
     });
   }
 
+  initSearchListener(): void {
+    this.searchSubject.pipe(
+      debounceTime(300), // Vrijeme čekanja (300ms)
+      distinctUntilChanged(), // Emittuje samo ako je vrijednost promijenjena,
+      map(q=> q.toLowerCase()),
+      filter(q => q.length > 3),
+      tap(q => console.log(this.dataSource.data.length))
+
+
+
+
+
+    ).subscribe((filterValue) => {
+      this.fetchStudents(filterValue, this.paginator.pageIndex + 1, this.paginator.pageSize);
+    });
+  }
+
   applyFilter(event: Event): void {
     const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
-    this.fetchStudents(filterValue, this.paginator.pageIndex + 1, this.paginator.pageSize);
+    this.searchSubject.next(filterValue); // Prosljeđuje vrijednost Subject-u
   }
 
   fetchStudents(filter: string = '', page: number = 1, pageSize: number = 5): void {
@@ -161,7 +181,7 @@ export class StudentsComponent implements OnInit, AfterViewInit {
 
   private restoreStudent(id: number) {
 
-    
+
     this.studentRestoreService.handleAsync(id).subscribe({
       next: () => {
         this.snackbar.showMessage('Student successfully restored.');
