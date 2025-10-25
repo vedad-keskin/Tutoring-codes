8e262791957998108ed221cf03345defcfe26552
Vedad Keskin <vedad.keskin98@gmail.com>
2025-10-25 14:12:49 +0200

    2A - Asja B.

--- Asja Bračaninović/11-10-2025 - RSI Prakticni/rs1-2025-02_11-novi - rjesenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
+++ Asja Bračaninović/11-10-2025 - RSI Prakticni/rs1-2025-02_11-novi - rjesenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-student-semesters',
   standalone: false,
-  
+
   templateUrl: './student-semesters.component.html',
   styleUrl: './student-semesters.component.css'
 })
 export class StudentSemestersComponent {
+
+
+
+  // Nase varijable
+  studentId: number = 0;
+
+  constructor( private route: ActivatedRoute,
+               private router: Router,
+                  ) {
+
+    this.studentId = route.snapshot.params['id'];
+
+  }
+
+
 
 }
