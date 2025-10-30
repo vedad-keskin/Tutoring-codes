ff06b56a15813d6e86a11f2332cc40f4f4de8df7
Vedad Keskin <vedad.keskin98@gmail.com>
2025-10-30 15:54:01 +0100

    RSI - Hasna Bunar

---
+++ Hasna Bunar/2. 08-10-2025 - RSI Ispitni/rs1-2025-02_11-novi - rjesenje/backend/RS1_2024_25.API/Endpoints/SemesterEndpoints/SemesterGetAllByStudentIdEndpoint.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RS1_2024_25.API.Data;
+using RS1_2024_25.API.Data.Models.SharedTables;
+using RS1_2024_25.API.Data.Models.TenantSpecificTables.Modul1_Auth;
+using RS1_2024_25.API.Helper.Api;
+using static RS1_2024_25.API.Endpoints.CityEndpoints.CityGetAll1Endpoint;
+using static RS1_2024_25.API.Endpoints.CityEndpoints.SemesterGetAllByStudentIdEndpoint;
+
+namespace RS1_2024_25.API.Endpoints.CityEndpoints;
+
+//bez paging i bez filtera
+[Route("semesters")]
+public class SemesterGetAllByStudentIdEndpoint(ApplicationDbContext db) : MyEndpointBaseAsync
+    .WithRequest<int>
+    .WithResult<SemesterGetAllByStudentIdResponse[]>
+{
+    [HttpGet("{id}")]
+    public override async Task<SemesterGetAllByStudentIdResponse[]> HandleAsync(int id,CancellationToken cancellationToken = default)
+    {
+        var result = await db.Semesters
+                        .Select(c => new SemesterGetAllByStudentIdResponse
+                        {
+                            ID = c.ID,
+                            AcademicYearName = c.AcademicYear!.Description,
+                            YearOfStudy = c.YearOfStudy,
+                            Renewal = c.Renewal,
+                            DateOfEnrollment = c.DateOfEnrollment,
+                            RecordedByName = c.RecordedBy!.Email,
+                        })
+                        .ToArrayAsync(cancellationToken);
+
+        return result;
+    }
+
+    public class SemesterGetAllByStudentIdResponse
+    {
+        public required int ID { get; set; }
+        public string? AcademicYearName { get; set; }
+        public int YearOfStudy { get; set; }
+        public bool Renewal { get; set; }
+        public DateTime DateOfEnrollment { get; set; }
+        public string? RecordedByName { get; set; }
+
+    }
+}
