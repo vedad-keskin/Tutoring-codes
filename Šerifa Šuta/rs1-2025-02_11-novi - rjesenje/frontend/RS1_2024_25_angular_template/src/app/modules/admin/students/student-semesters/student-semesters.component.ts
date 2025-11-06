552d3de9b65103ddb22d770409135afcad253491
Vedad Keskin <vedad.keskin98@gmail.com>
2025-11-06 22:39:57 +0100

    new code - Serifa S.

--- Šerifa Šuta/rs1-2025-02_11-novi - rjesenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
+++ Šerifa Šuta/rs1-2025-02_11-novi - rjesenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
@@ -4,6 +4,9 @@ import {
   StudentGetByIdEndpointService, StudentGetByIdResponse
 } from '../../../../endpoints/student-endpoints/student-get-by-id-endpoint.service';
 import {MySnackbarHelperService} from '../../../shared/snackbars/my-snackbar-helper.service';
+import {
+  SemesterGetAllByStudentIdEndpoint
+} from '../../../../endpoints/semester-endpoints/semester-get-all-by-student-id-endpoint.service';
 
 @Component({
   selector: 'app-student-semesters',
@@ -19,12 +22,17 @@ export class StudentSemestersComponent implements OnInit {
 
    //student:any;
    student: StudentGetByIdResponse | null = null;
+   semesters:any;
+
+  displayedColumns: string[] = ['id', 'academicYear', 'yearOfStudy', 'renewal','winterSemester','recordedBy'];
+
 
 
   constructor(    private route: ActivatedRoute,
                   private router: Router,
                   private studentGetByIdService:StudentGetByIdEndpointService,
                   private snackbar: MySnackbarHelperService,
+                  private semesterGetAllByStudentIdService:SemesterGetAllByStudentIdEndpoint
 
   ) {
 
@@ -35,6 +43,7 @@ export class StudentSemestersComponent implements OnInit {
   ngOnInit(): void {
 
     this.fetchStudent();
+    this.fetchSemesters();
 
     }
 
@@ -56,4 +65,26 @@ export class StudentSemestersComponent implements OnInit {
   }
 
 
+  private fetchSemesters() {
+
+    this.semesterGetAllByStudentIdService.handleAsync(this.studentId).subscribe({
+      next: (data) => {
+
+        this.semesters = data;
+
+      },
+      error: (err) => {
+        this.snackbar.showMessage('Error fetching semesters. Please try again.', 5000);
+        console.error('Error fetching semesters:', err);
+      }
+    });
+
+  }
+
+  navigateToNewSemester() {
+
+    this.router.navigate(['/admin/students/semesters/new', this.studentId]);
+
+
+  }
 }
