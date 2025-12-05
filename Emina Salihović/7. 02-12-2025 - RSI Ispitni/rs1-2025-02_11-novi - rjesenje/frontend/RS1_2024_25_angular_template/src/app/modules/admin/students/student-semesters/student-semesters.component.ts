cb1a38c58224371ae7097abd0675bbf5e652cc6f
Vedad Keskin <vedad.keskin98@gmail.com>
2025-12-05 20:21:18 +0100

    Student info - Emina S.

--- Emina Salihović/7. 02-12-2025 - RSI Ispitni/rs1-2025-02_11-novi - rjesenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
+++ Emina Salihović/7. 02-12-2025 - RSI Ispitni/rs1-2025-02_11-novi - rjesenje/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {
+  StudentGetByIdEndpointService
+} from '../../../../endpoints/student-endpoints/student-get-by-id-endpoint.service';
+import {MySnackbarHelperService} from '../../../shared/snackbars/my-snackbar-helper.service';
 
 @Component({
   selector: 'app-student-semesters',
   standalone: false,
-  
+
   templateUrl: './student-semesters.component.html',
   styleUrl: './student-semesters.component.css'
 })
-export class StudentSemestersComponent {
+export class StudentSemestersComponent implements OnInit {
 
+
+  studentId: number = 0;
+  student:any;
+
+constructor(  private route: ActivatedRoute,
+              private router: Router,
+              private studentGetByIdService:StudentGetByIdEndpointService,
+              private snackbar: MySnackbarHelperService,
+              ) {
+
+  this.studentId = route.snapshot.params['id'];
+
+}
+
+  ngOnInit(): void {
+
+  this.GetStudent();
+
+    }
+
+
+  private GetStudent() {
+
+    this.studentGetByIdService.handleAsync(this.studentId).subscribe({
+      next: (data) => {
+        this.snackbar.showMessage('Student successfully fetched.');
+
+        this.student = data;
+
+      },
+      error: (err) => {
+        this.snackbar.showMessage('Error fetching student. Please try again.', 5000);
+        console.error('Error fetching student:', err);
+      }
+    });
+
+
+  }
 }
