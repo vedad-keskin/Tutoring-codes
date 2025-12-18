7a288931f2870af9793ea4adc4731929fe971731
Vedad Keskin <vedad.keskin98@gmail.com>
2025-12-18 20:26:43 +0100

    Backend endpointi - Ajdin A.

---
+++ Ajdin Alić/2. 08-12-2025 - RSI Ispitni/rs1-2025-02_11-novi - rjesenje/backend/RS1_2024_25.API/Endpoints/SemesterEndpoints/SemesterUpdateOrInsertValidator.cs
@@ -0,0 +1,24 @@
+﻿namespace RS1_2024_25.API.Endpoints.CityEndpoints;
+
+using FluentValidation;
+using RS1_2024_25.API.Data;
+
+public class SemesterUpdateOrInsertValidator : AbstractValidator<SemesterUpdateOrInsertEndpoint.SemesterUpdateOrInsertRequest>
+{
+    public SemesterUpdateOrInsertValidator(ApplicationDbContext dbContext)
+    {
+
+
+        // Validacija Price
+        RuleFor(x => x.Price)
+            .GreaterThanOrEqualTo(50).WithMessage("Price must be higher than 50.")
+            .LessThanOrEqualTo(2000).WithMessage("Price must be lower than 2000.");
+
+
+        //RuleFor(x => x.Price)
+        //    .InclusiveBetween(50,2000).WithMessage("Price must be between 50 and 2000.");
+
+
+
+    }
+}
