6be476360969f80cbe8bd4d6136e5cde1004d906
Vedad Keskin <vedad.keskin98@gmail.com>
2026-01-19 16:20:48 +0100

    Semester klasa

--- Ajla Stranjak i kolegice/rs1-2025-02_11-novi - rjesenje/backend/RS1_2024_25.API/Helper/BaseClasses/TenantSpecificTable.cs
+++ Ajla Stranjak i kolegice/rs1-2025-02_11-novi - rjesenje/backend/RS1_2024_25.API/Helper/BaseClasses/TenantSpecificTable.cs
@@ -9,8 +9,8 @@ using System.Threading.Tasks;
 namespace RS1_2024_25.API.Helper.BaseClasses;
 
 public abstract class TenantSpecificTable : BaseEntity
-{
-    public int TenantId { get; set; }
+{    public int TenantId { get; set; }
     [ForeignKey(nameof(TenantId))]
     public Tenant? Tenant { get; set; }
+
 }
