88dd13ec8665d8ffdb7320b4ad0f9c4df4843775
Vedad Keskin <vedad.keskin98@gmail.com>
2026-01-21 19:39:34 +0100

    Frontend endpointi AZ

---
+++ AZ/rs1-2025-02_11-novi - postavka/frontend/RS1_2024_25_angular_template/src/app/endpoints/semester-endpoint/semester-get-student-by-id-endpoint.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {MyConfig} from '../../my-config';
+import {MyBaseEndpointAsync} from '../../helper/my-base-endpoint-async.interface';
+
+export interface SemesterGetByStudentIdResponse {
+  id: number;
+  winterSemester:Date;
+  yearOfStudy:number;
+  renewal:boolean;
+  academicYearName: string;
+  recordedByName: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SemesterGetByStudentIdEndpoint implements MyBaseEndpointAsync<number, SemesterGetByStudentIdResponse> {
+  private apiUrl = `${MyConfig.api_address}/semesters`;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  handleAsync(id: number) {
+    return this.httpClient.get<SemesterGetByStudentIdResponse>(`${this.apiUrl}/${id}`);
+  }
+}
