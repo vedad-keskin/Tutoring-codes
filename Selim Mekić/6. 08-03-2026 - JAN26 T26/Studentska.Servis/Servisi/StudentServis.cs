5ffddeaa50703e2f8f5e9ad0bcf07318c9e5df06
Vedad Keskin <vedad.keskin98@gmail.com>
2026-03-08 14:54:04 +0100

    Add Studentska solution with Data, Service, WinApp
    Initial commit adding a Studentska solution and projects. Introduces Studentska.Data (entity models: Student, Grad, Drzava, Spol, Predmet, AkademskaGodina; net8.0 csproj), Studentska.Servis (EF Core Sqlite DbContext, BaseServis and typed services with common queries, csproj), and Studentska.WinApp (WinForms helpers: Generator, Validator, ImageHelper, Resursi, Ekstenzije, keys, reports dataset, forms, App.config with SQLite connection). Also adds .gitignore, README, solution file and an embedded Studentska.db SQLite file. This provides the base domain, data access and UI helpers to continue development.

---
+++ Selim Mekić/6. 08-03-2026 - JAN26 T26/Studentska.Servis/Servisi/StudentServis.cs
@@ -0,0 +1,14 @@
+﻿using Studentska.Data.Entiteti;
+
+namespace Studentska.Servis.Servisi
+{
+    public class StudentServis : BaseServis<Student>
+    {
+
+        public int GetBrojStudenata()
+        {
+            return _dbContext.Studenti.Count();
+        }
+
+    }
+}
