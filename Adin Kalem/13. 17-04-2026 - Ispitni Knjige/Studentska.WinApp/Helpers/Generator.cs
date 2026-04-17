0816e7e34ee2fcf8c13975a22775abf96890a77c
Vedad Keskin <vedad.keskin98@gmail.com>
2026-04-17 17:07:00 +0200

    Add Studentska solution with WinApp, data, service
    Initial import of the Studentska exam project: adds solution and three projects (Studentska.Data, Studentska.Servis, Studentska.WinApp). Data project (net8.0) contains entity models (Student, Grad, Drzava, Spol, Predmet, AkademskaGodina) and a csproj. Servis project (net8.0) includes StudentskaDbContext (EF Core SQLite), generic BaseServis and specific services (Student, Grad, Drzava, Spol, Predmet, AkademskaGodina) plus project references and package dependencies. WinApp includes WinForms app files, helpers (validation, image, generators, resources, extensions), report dataset files, App.config with connection string and an included Studentska.db. Also adds a comprehensive .gitignore and a simple README. This commit provides the full starting codebase for the exam application.

---
+++ Adin Kalem/13. 17-04-2026 - Ispitni Knjige/Studentska.WinApp/Helpers/Generator.cs
@@ -0,0 +1,25 @@
+﻿using Studentska.Servis;
+using Studentska.Servis.Servisi;
+
+using System.Text;
+
+namespace Studentska.WinApp.Helpers
+{
+    public class Generator
+    {
+        public static string GenerisiIndeks()
+        {
+            return $"IB{((DateTime.Now.Year - 2000) * 10000) + new StudentServis().GetBrojStudenata() + 1}";
+        }
+        public static string GenerisiLozinku(int brojZnakova = 10)
+        {
+            string dozvoljniZnakovi = "abcdefghijkljmnoprstuvzwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+            Random rand = new Random();
+            StringBuilder lozinka = new StringBuilder();//xQ1!2@abC3
+            for (int i = 0; i < brojZnakova; i++)
+                lozinka.Append(dozvoljniZnakovi[rand.Next(dozvoljniZnakovi.Length)]);
+            return lozinka.ToString();
+        }
+    }
+
+}
