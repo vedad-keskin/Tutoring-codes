56673a2d191407743223caeeebabd3aaad46a2ac
Vedad Keskin <vedad.keskin98@gmail.com>
2026-07-21 20:38:02 +0200

    c++ code

---
+++ Amin Alešević i Zejd Hadžić/18. 20-07-2026 - PRIII Ispitni Projekti/Studentska.Servis/Servisi/GradServis.cs
@@ -0,0 +1,17 @@
+﻿using Studentska.Data.Entiteti;
+
+namespace Studentska.Servis.Servisi
+{
+    public class GradServis : BaseServis<Grad>
+    {        
+        public List<Grad> GetByNaziv(string nazivGrada)
+        {
+            return _dbContext.Gradovi.Where(grad => grad.Naziv.Contains(nazivGrada)).ToList();
+        }
+
+        public List<Grad> GetByDrzavaId(int drzavaId)
+        {
+            return _dbContext.Gradovi.Where(grad => grad.DrzavaId == drzavaId).ToList();
+        }    
+    }
+}
